88b5bac4c94a9d02a56fe3e86ade7aefba8bb742
Juan Gelpud <juanr970318@gmail.com>
2021-10-11 20:09:35 -0500

    Remove blank space

--- src/main/java/com/jg/apirest/APIREST/ApiRestApplication.java
+++ src/main/java/com/jg/apirest/APIREST/ApiRestApplication.java
@@ -1,7 +1,15 @@
 package com.jg.apirest.APIREST;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.DateFormat;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @SpringBootApplication
 public class ApiRestApplication {
